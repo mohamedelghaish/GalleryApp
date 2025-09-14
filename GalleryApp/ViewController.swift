01ad6e082c10478354782cc12a5a102cca0d5132
Mohamed Kotb <kotbm055@gmail.com>
2025-09-15 00:25:08 +0300

    Initial Commit

---
+++ GalleryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GalleryApp
+//
+//  Created by Mohamed Kotb on 15/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
