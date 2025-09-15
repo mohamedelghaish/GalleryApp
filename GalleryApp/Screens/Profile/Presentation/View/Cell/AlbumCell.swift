1426de86118efb794afc6529a9b9fe6f14a8f046
Mohamed Kotb <kotbm055@gmail.com>
2025-09-15 22:24:08 +0300

    implement profile screen UI

---
+++ GalleryApp/Screens/Profile/Presentation/View/Cell/AlbumCell.swift
@@ -0,0 +1,25 @@
+//
+//  AlbumCell.swift
+//  GalleryApp
+//
+//  Created by Mohamed Kotb on 15/09/2025.
+//
+
+import UIKit
+
+class AlbumCell: UITableViewCell {
+
+    @IBOutlet weak var albumName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ GalleryApp/Screens/Profile/Presentation/View/ProfileViewController.swift
@@ -0,0 +1,76 @@
+//
+//  ProfileViewController.swift
+//  GalleryApp
+//
+//  Created by Mohamed Kotb on 15/09/2025.
+//
+
+import UIKit
+import Combine
+final class ProfileViewController: UIViewController {
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userAddress: UILabel!
+    @IBOutlet weak var albumTabelView: UITableView!
+    
+    private let viewModel = ProfileViewModel()
+    private var cancellables = Set<AnyCancellable>()
+    
+    // MARK: - Init with nib
+    init() {
+        super.init(nibName: "ProfileViewController", bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        bindViewModel()
+        viewModel.loadUser()
+    }
+    
+    private func setupUI() {
+        albumTabelView.delegate = self
+        albumTabelView.dataSource = self
+        albumTabelView.registerNib(AlbumCell.self)
+    }
+    
+    private func bindViewModel() {
+        viewModel.$user
+            .sink { [weak self] user in
+                guard let user = user else { return }
+                self?.userName.text = user.name
+                self?.userAddress.text = "\(user.address.street), \(user.address.suite), \(user.address.city), \(user.address.zipcode)"
+            }
+            .store(in: &cancellables)
+        
+        viewModel.$albums
+            .sink { [weak self] _ in
+                DispatchQueue.main.async {
+                    self?.albumTabelView.reloadData()
+                }
+            }
+            .store(in: &cancellables)
+    }
+}
+
+
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.albums.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeue(AlbumCell.self, for: indexPath)
+        cell.albumName?.text = viewModel.albums[indexPath.row].title
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let album = viewModel.albums[indexPath.row]
+        let albumVC = AlbumViewController(albumId: album.id, albumTitle: album.title)
+        navigationController?.pushViewController(albumVC, animated: true)
+    }
+}
